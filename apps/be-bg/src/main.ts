3f4199db1545d6784dfe5cb5ada92a42b4a91e76
pierre <pro.pierre.vidal@gmail.com>
2025-11-19 11:14:07 +0100

    swagger

--- apps/be-bg/src/main.ts
+++ apps/be-bg/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Configuration Swagger
+  const config = new DocumentBuilder()
+    .setTitle('CinePotes API')
+    .setDescription('Documentation de l\'API CinePotes')
+    .setVersion('1.0')
+    .addTag('films') // Optionnel : ajoutez des tags
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
---
+++ apps/be-bg/src/test.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+
+@ApiTags('Test')
+@Controller('test')
+export class TestController {
+  
+  @Get()
+  @ApiOperation({ summary: 'Test simple' })
+  @ApiResponse({ status: 200, description: 'Ça marche !' })
+  test() {
+    return { message: 'Hello Swagger!' };
+  }
+
+  @Get('hello')
+  @ApiOperation({ summary: 'Dire bonjour' })
+  hello() {
+    return { message: 'Bonjour depuis CinePotes!' };
+  }
+}
