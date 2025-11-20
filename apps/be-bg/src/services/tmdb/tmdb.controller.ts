4d75c80046c24bff73a54a956c27c67eb8d2699b
Mehdi <mehdilaz109@gmail.com>
2025-11-20 21:57:34 +0100

    Creation du controller pour recuperer un film par son id tmdb

--- apps/be-bg/src/services/tmdb/tmdb.controller.ts
+++ apps/be-bg/src/services/tmdb/tmdb.controller.ts
@@ -1,7 +1,21 @@
 import { Controller } from '@nestjs/common';
+import { Get } from '@nestjs/common';
+import { Param } from '@nestjs/common';
+import { ParseIntPipe } from '@nestjs/common';
+import { DetailsFilm } from './types/tmdb.types';
 import { TmdbService } from './tmdb.service';
 
 @Controller('tmdb')
 export class TmdbController {
   constructor(private readonly tmdbService: TmdbService) {}
+
+  /**
+  * Methode pour obtenir les détails d'un film par son identifiant TMDB.
+  * @param id - Identifiant TMDB du film
+  * @returns Détails du film
+  */
+  @Get(':id')
+  getMovie(@Param('id', ParseIntPipe) id: number): Promise<DetailsFilm> {
+    return this.tmdbService.obtenirDetailsFilm(id);
+  }
 }
--- apps/be-bg/src/services/tmdb/tmdb.module.ts
+++ apps/be-bg/src/services/tmdb/tmdb.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TmdbService } from './tmdb.service';
 import { TmdbController } from './tmdb.controller';
+import { RedisModule } from '../redis/redis.module';
 
 @Module({
+  imports: [RedisModule],
   controllers: [TmdbController],
   providers: [TmdbService],
 })
