349e69cee963bf84357a9a2a1d5e958ee1e6cfa9
michal <michal781998@protonmail.com>
2026-01-16 15:27:36 +0100

    fix: fix page Link

--- apps/fe-client/components/utils.tsx
+++ apps/fe-client/components/utils.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export interface DetailsFilm {
   id: number;
@@ -21,25 +22,25 @@ function Header() {
             <div className="px-4">
                 <div className="flex items-center justify-between">
                     <div className="flex shrink-0">
-                        <a aria-current="page" className="flex items-center" href="/">
-                            <img className="h-7 w-auto" src="https://img.icons8.com/?size=100&id=11860&format=png&color=000000" alt="CinePotes" />
-                            <p className="sr-only">Website Title</p>
-                        </a>
+                      <Link href="/" className="flex items-center" aria-current="page">
+                          <img className="h-7 w-auto" src="https://img.icons8.com/?size=100&id=11860&format=png&color=000000" alt="CinePotes" />
+                          <p className="sr-only">Website Title</p>
+                      </Link>
                     </div>
                     <div className="hidden md:flex md:items-center md:justify-center md:gap-5">
-                        <a aria-current="page"
+                        <Link aria-current="page"
                             className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-                            href="/">Home</a>
-                            <a className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-                            href="/Contact">Contact</a>
-                        <a className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-                            href="/About">About us</a>
+                            href="/">Home</Link>
+                        <Link className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+                            href="/Contact">Contact</Link>
+                        <Link className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+                            href="/About">About us</Link>
                     </div>
                     <div className="flex items-center justify-end gap-3">
-                        <a className="hidden items-center justify-center rounded-xl bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-all duration-150 hover:bg-gray-50 sm:inline-flex"
-                            href="/login">Sign in</a>
-                        <a className="inline-flex items-center justify-center rounded-xl bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-                            href="/login">Login</a>
+                        <Link className="hidden items-center justify-center rounded-xl bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-all duration-150 hover:bg-gray-50 sm:inline-flex"
+                            href="/login">Sign in</Link>
+                        <Link className="inline-flex items-center justify-center rounded-xl bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                            href="/login">Login</Link>
                     </div>
                 </div>
             </div>
