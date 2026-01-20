b71c8df01e2c46ce9de42a6f1f5e88cd61172865
ALLAOUI Ahmed <ahmed.allaoui@etu.unilim.fr>
2026-01-20 15:11:48 +0100

    fix: changement des jdd dans init

--- init.sql
+++ init.sql
@@ -98,7 +98,7 @@ CREATE INDEX idx_utilisateur_email ON utilisateur(email);
 -- NOSONAR - Test data for local development only
 INSERT INTO utilisateur (nom, email, mot_de_passe_hash, role)
 VALUES
-('Admin CinéPote', 'admin@cinepote.fr', '$2b$10$M7sxjSMT2OBvIgGaVxhxpe18gYetRw2Q3xv/gxbn1O5fy9vS4vZtS', 'admin'),
+('Admin CinéPote', 'admin@cinepote.fr', 'test', 'admin'),
 -- NOSONAR - Test data for local development only
-('Max Dupont', 'max.chef@cinepote.fr', '$2b$10$nOjGULHNYuWMGteTF7WA.hmRqTECq/Q/7A0EYpt4oMojbrbE.4yUO', 'chef'),
-('August Martin', 'august.user@cinepote.fr', '$2b$10$W7.emBmUK7QMGFCnH7Dhguz5WZhTwMu8lY3DjUUCJmi7tOXuqqs8e', 'user');
+('Max Dupont', 'max.chef@cinepote.fr', 'kalilinux', 'chef'),
+('August Martin', 'august.user@cinepote.fr', 'Ryzen2025', 'user');
