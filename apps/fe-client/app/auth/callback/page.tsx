c1f3f79bf509488bfcf9f69e5bbedda5f9c19e5f
Mehdi <mehdilaz109@gmail.com>
2026-01-22 14:31:08 +0100

    fix: navigation et recuperation token

--- apps/fe-client/app/auth/callback/page.tsx
+++ apps/fe-client/app/auth/callback/page.tsx
@@ -1,28 +1,13 @@
 "use client";
-import { useEffect } from "react";
-import { useSearchParams, useRouter } from "next/navigation";
-/*
-Recuperation du token a partir de l url
-stocker dans local storage
-redirection vers fe client app.page.tsx */
-export default function AuthCallbackPage() {
-  const params = useSearchParams();
-  const router = useRouter();
 
-  useEffect(() => {
-    const token = params.get("token");
+import { Suspense } from "react";
+import CallbackClient from "./callback-client";
 
-    if (token) {
-      localStorage.setItem("access_token", token);
-      router.replace("/");
-    } else {
-      router.replace("/");
-    }
-  }, [params, router]);
-
+export default function Page() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <p>Connexion en cours...</p>
-    </div>
+    <Suspense fallback={<div className="p-6">Connexion en cours...</div>}>
+      <CallbackClient />
+    </Suspense>
   );
 }
+
