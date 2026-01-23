334261e1067ad09d5e61133eb689eb51a9e5ff20
remi tardif <remitardif11@gmail.com>
2026-01-23 16:09:04 +0100

    fix: bug test service list

--- apps/be-bg/src/lists/lists.module.ts
+++ apps/be-bg/src/lists/lists.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ListsController } from './lists.controller';
-import { ListsService } from './lists.service';
+import { ListesService } from './lists.service';
 import { Liste } from './entities/liste.entity';
 import { ListeFilm } from './entities/liste-film.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Liste, ListeFilm])],
   controllers: [ListsController],
-  providers: [ListsService],
-  exports: [ListsService],
+  providers: [ListesService],
+  exports: [ListesService],
 })
 export class ListsModule {}
